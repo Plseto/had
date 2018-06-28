f35752a1776a9f27f97e691fed5c2c51ce65ce51
Plseto <34749085+Plseto@users.noreply.github.com>
2018-06-28 19:30:08 +0200

    Add files via upload

--- had 6.py
+++ had 6.py
@@ -10,79 +10,68 @@ from Communication.tcp import Client
 ##i=soubor.read()
 ##soubor.close()
 
-server = Client('192.168.42.71',1234)
+server = Client('localhost',12345)
 
 grid_size = 10
 
-mapa = [[[]]]
+mapa = []
 x = 0
 y = 0
 frame = 0
 
-##if server.connected == False:
-##    print('Nepodarilo se pripojit.')
-##else:
+if server.connected == False:
+    print('Nepodarilo se pripojit.')
+else:
+    server.timeout = 0
+    pygame.init()
 
-pygame.init()
+    screen = pygame.display.set_mode((800,800))
 
-screen = pygame.display.set_mode((800,800))
+    pygame.time.set_timer(pygame.USEREVENT + 1,100)
 
-pygame.time.set_timer(pygame.USEREVENT + 1,1000)
 
-soubor = open('snake.txt')
-a=soubor.read()
-soubor.close()
-for p in a:
-    if (p=='!'):
-        mapa[-1][-1].append('!')
-        mapa.append([])
-    elif (p== '\n'):
-        mapa[-1].append([])
-        mapa[-1][-1].append('\n')
-    else:
-        mapa[-1][-1].append(p)
-
-while True:
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_END):
-        server.close()
-        break
-    if event.type == pygame.KEYDOWN:
-        if pygame.key.get_pressed()[pygame.K_w]:
-            server.write('w')
-        elif pygame.key.get_pressed()[pygame.K_a]:
-            server.write('a')
-        elif pygame.key.get_pressed()[pygame.K_s]:
-            server.write('s')
-        elif pygame.key.get_pressed()[pygame.K_d]:
-            server.write('d')
-    if event.type == (pygame.USEREVENT + 1):
-        screen.fill((0,0,0))
-        for line in mapa[frame]:
-            for i in line:
-                if i == '\n':
+    while True:
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_END):
+            server.close()
+            break
+        if event.type == pygame.KEYDOWN:
+            if pygame.key.get_pressed()[pygame.K_w]:
+                server.write('w')
+            elif pygame.key.get_pressed()[pygame.K_a]:
+                server.write('a')
+            elif pygame.key.get_pressed()[pygame.K_s]:
+                server.write('s')
+            elif pygame.key.get_pressed()[pygame.K_d]:
+                server.write('d')
+        if event.type == (pygame.USEREVENT + 1):
+            while True:
+                i = server.read()
+                if not i:
+                    break
+                if i == b'\n':
                     x=0
                     y+=grid_size
-                elif i == '!':
+                elif i == b'!':
                     x=0
                     y=0
                     pygame.display.flip()
-                    frame += 1
-                elif i == '#':
+                    screen.fill((0,0,0))
+                elif i == b'#':
                     pygame.draw.rect(screen, (0,0,255),(x,y,grid_size,grid_size))
                     x+=grid_size
-                elif i == '*':
+                elif i == b'*':
                     pygame.draw.circle(screen, (255,0,0),(x+grid_size // 2,y+grid_size // 2),grid_size // 2)
                     x+=grid_size
-                elif i == ' ':
+                elif i == b' ':
                     x+=grid_size
-                elif i == 'a':
+                elif i == b'a':
                     pygame.draw.circle(screen, (255,255,0),(x+grid_size // 2,y+grid_size // 2),grid_size // 2)
                     x+=grid_size
-                elif i == 'b':
+                elif i == b'b':
                     pygame.draw.circle(screen, (0,255,0),(x,y),grid_size // 2)
                     x+=grid_size
-                elif i == 'c':
+                elif i == b'c':
                     pygame.draw.circle(screen, (255,255,255),(x,y),grid_size // 2)
                     x+=grid_size
 
